b086a7a28a62f0708c8dd93fbe1eea153a4c9657
vitormanfre <vitor.manfre@tegra.me>
2019-04-12 09:47:14 -0300

    removes unecessary interfaces

--- lib/interfaces/plug.ts
+++ lib/interfaces/plug.ts
@@ -1,13 +1,3 @@
 import { Observable } from "rxjs";
 
 export type TStream = (stream: any) => Observable<any>;
-
-export interface ILifecycle {
-  componentDidMount: () => void;
-  componentWillUnmount: () => void;
-}
-
-export interface IPlugState {
-  hasEmmited: boolean;
-  innerData?: any;
-}
